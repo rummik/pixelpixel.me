b7e16f6ccbbb23342f2cadfec5bc9b6e1bfc479d
rummik <r@9k1.us>
2014-04-06 21:28:43 -0400

    Display current pen color

--- js/pallet.js
+++ js/pallet.js
@@ -2,9 +2,22 @@
 'use strict';
 
 function Pallet(pxpx) {
+	var self = this;
+
 	this.pxpx = pxpx;
 
 	this.ui = document.createElement('div');
+	this.ui.className = 'pallet';
+
+	this.pen = document.createElement('div');
+	this.ui.appendChild(this.pen);
+	this.pen.className = 'pen';
+
+	pxpx.on('penchange', function() {
+		self.pen.style.background = self.pxpx.pen;
+	});
+
+	pxpx.emit('penchange');
 
 	pxpx.on('layerchange', function() {
 		// show new pallet
